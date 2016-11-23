bedc66c2c8c2b434900157fb6b9d01d089e10b1e
jpelkey4725 <jpelkey4725@users.noreply.github.com>
2016-11-23 12:47:10 -0700

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,33 @@
+
+  ## This function will cache a given matrix that is passed to the function
+  
+  makeCacheMatrix <- function(x = matrix()) {
+    
+      inv <- NULL
+      set <- function(y) {
+        x <<- y
+        inv <<- NULL
+      }
+      get <- function() x
+      
+      setinv <- function(solve) inv <<- solve
+      getinv <- function() inv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
+  
+    }
+  
+  
+cacheInv <- function(x, ...) {
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix <- x$get()
+  inv <- solve(matrix, ...)
+  x$setinv(inv)
+  inv
+  ## return inverse of matrix 'inv'
+}
